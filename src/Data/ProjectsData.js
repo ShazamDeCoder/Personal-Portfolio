f939567880c6262caeb574c123db5090d238602b
Adio Mubarak Adebukola <adiomubarakadebukola2026@gmail.com>
2026-01-11 12:47:47 +0100

    Add new portfolio project

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -1,11 +1,22 @@
 const projects = [
   {
     id: 1,
+    title: "Saas Landing Page",
+    description:
+      "A pixel-perfect SaaS landing experience with React, TailwindCSS, and React Router. Includes a hero section, pricing, testimonials, login/signup pages, and a fully responsive layout",
+    image:"https://res.cloudinary.com/dvxoylstd/image/upload/v1768131214/blinky_sgid0a.png" ,
+    technologies: ["React","Vite", "Tailwind CSS"],
+    liveUrl: "https://blinky-saas.vercel.app",
+    githubUrl: "https://github.com/ShazamDeCoder/Blinky-saas.git",
+    featured: false,
+  },
+  {
+    id: 2,
     title: "Coffee Website",
     description:
       "A coffee website built with React. Features include user authentication, product catalog, shopping cart, payment integration, and admin dashboard.",
     image:"https://res.cloudinary.com/dvxoylstd/image/upload/v1767837474/Capture_kqmcas.png" ,
-    technologies: ["React js", "Tailwind CSS"],
+    technologies: ["React", "Vite", "Tailwind CSS"],
     liveUrl: "https://espressio-coffee.vercel.app",
     githubUrl: "https://github.com/ShazamDeCoder/Espressio-Coffee.git",
     featured: false,
